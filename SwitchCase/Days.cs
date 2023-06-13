f34230840127073aaab32f7bf75aaaf3deabb145
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-06-13 12:53:35 +0530

    Add project files.

---
+++ SwitchCase/Days.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    internal class Days
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter your choice");
+            num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1.Monday");
+            Console.WriteLine("2.Tuesday");
+            Console.WriteLine("3.Wednesday");
+            Console.WriteLine("4.Thursday");
+            Console.WriteLine("5.Friday");
+            Console.WriteLine("6.Saturday");
+            Console.WriteLine("7.Sunday");
+            Console.WriteLine("Select your chioce");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Day is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Day is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Day is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Day is Thursday");
+                    break; ;
+                case 5:
+                    Console.WriteLine("Day is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Day is Saturday");
+                    break;  
+                case 7:
+                    Console.WriteLine("Day is Sunday");
+                break;
+
+            }
+        }
+    }
+}
---
+++ SwitchCase/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//int number;
+//Console.WriteLine("Enter num1");
+//int n1= Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter num2");
+//int n2= Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("1.Addition");
+//Console.WriteLine("2.Subtraction");
+//Console.WriteLine("3.Multiplication");
+//Console.WriteLine("4.Subtraction");
+//Console.WriteLine("5.Module");
+//Console.WriteLine("Select any one option");
+//int option=Convert.ToInt32(Console.ReadLine());
+//switch (option)
+//{
+//    case 1:
+//        int sum = n1 + n2;
+//        Console.WriteLine("Addition" + sum);
+//        break;
+
+//    case 2:
+//        int sub = n1 - n2;
+//        Console.WriteLine("subtraction=" + sub);
+//        break;
+//    case 3:
+//        int mul = n1 * n2;
+//        Console.WriteLine("Multiplication" + mul);
+//        break;
+//    case 4:
+//        double div = n1 / n2;
+//        Console.WriteLine("Division=" + div);
+//        break;
+//    case 5:
+//        bool div2 = n1 / n2 == 0;
+//        Console.WriteLine("Module" + div2);
+//        break;
+//}
